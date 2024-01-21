5d03d29c33f6ac7803b069b21c4c86eaa0b96160
Koby Fink <finkkoby@icloud.com>
2024-01-21 11:15:12 -0500

    complete logo

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ window.onload = function() {
     selfie.addEventListener('click', function() {
         selfie.style = "border-color:white;";
     });
+    const logo = document.getElementById("initals");
+    logo.addEventListener('mouseover', function() {
+        logo.style = "opacity:100%";
+    });
+    logo.addEventListener('mouseout', function() {
+        logo.style = "opacity:50%";
+    })
 }
