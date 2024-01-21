61b6ed2c9a69e200ad6c75676b7daef6f6282cf2
Koby Fink <finkkoby@icloud.com>
2024-01-21 10:32:11 -0500

    basic structure and formatting done

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const selfie = document.getElementById("selfie");
+    selfie.addEventListener('click', function() {
+        selfie.style = "border-color:white;";
+    });
+}
